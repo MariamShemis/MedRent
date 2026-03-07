2eff8cc242fa9f165f5b509f41a53a6b3ed569e6
asila3172 <sosonabil317@gmail.com>
2026-03-07 17:34:09 +0200

    push profile

--- lib/features/main_layout/profile/presentation/view/profile_tab.dart
+++ lib/features/main_layout/profile/presentation/view/profile_tab.dart
@@ -1,7 +1,9 @@
+
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:image_picker/image_picker.dart'; 
 import 'package:med_rent/core/constants/color_manager.dart';
 import 'package:med_rent/core/routes/app_routes.dart';
 import 'package:med_rent/features/language/data/cubit/app_localization_cubit.dart';
@@ -23,21 +25,51 @@ class _ProfileTabState extends State<ProfileTab> {
   String userName = "";
   int userId = 0;
 
-  bool isLoadingUserData = false;
-
-  Future<void> _loadUserData() async {
-    context.read<ProfileCubit>().getProfileData();
+ 
+  Future<void> _pickImage(ImageSource source) async {
+    final ImagePicker picker = ImagePicker();
+    final XFile? image = await picker.pickImage(source: source);
+    if (image != null) {
+      
+      debugPrint("Image Path: ${image.path}");
+    }
   }
 
-  @override
-  void initState() {
-    super.initState();
-    _loadUserData();
+  void _showImageSourcePicker(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(20.r)),
+      ),
+      builder: (context) => SafeArea(
+        child: Wrap(
+          children: [
+            ListTile(
+              leading: const Icon(Iconsax.gallery, color: ColorManager.darkBlue),
+              title: const Text('Gallery'),
+              onTap: () {
+                Navigator.pop(context);
+                _pickImage(ImageSource.gallery);
+              },
+            ),
+            ListTile(
+              leading: const Icon(Iconsax.camera, color: ColorManager.darkBlue),
+              title: const Text('Camera'),
+              onTap: () {
+                Navigator.pop(context);
+                _pickImage(ImageSource.camera);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     AppLocalizations appLocalizations = AppLocalizations.of(context)!;
+    
     return BlocProvider(
       create: (context) => ProfileCubit(ProfileData())..getProfileData(),
       child: Scaffold(
@@ -50,151 +82,131 @@ class _ProfileTabState extends State<ProfileTab> {
                   Text(
                     appLocalizations.profile,
                     textAlign: TextAlign.center,
-                    style: Theme.of(
-                      context,
-                    ).textTheme.headlineLarge!.copyWith(fontSize: 24.sp),
+                    style: Theme.of(context).textTheme.headlineLarge!.copyWith(fontSize: 24.sp),
                   ),
                   SizedBox(height: 15.h),
+
                   BlocBuilder<ProfileCubit, ProfileState>(
                     builder: (context, state) {
-                      if (isLoadingUserData || state is ProfileLoading) {
+                      if (state is ProfileLoading) {
+                        return SizedBox(
+                          height: 150.h,
+                          child: const Center(child: CircularProgressIndicator()),
+                        );
+                      }
+
+                      if (state is ProfileError) {
                         return Column(
                           children: [
-                            UserImageProfile(
-                              widgetUserImageProfile: CircleAvatar(
-                                radius: 46.r,
-                                backgroundImage:
-                                    currentImageUrl != null &&
-                                        currentImageUrl!.isNotEmpty
-                                    ? NetworkImage(
-                                        "$currentImageUrl?t=${DateTime.now().millisecondsSinceEpoch}",
-                                      )
-                                    : null,
-                                child:
-                                    currentImageUrl == null ||
-                                        currentImageUrl!.isEmpty
-                                    ? Icon(Icons.person, size: 40.sp)
-                                    : null,
-                              ),
+                            Text('Error: ${state.message}'),
+                            ElevatedButton(
+                              onPressed: () => context.read<ProfileCubit>().getProfileData(),
+                              child: const Text('Retry'),
                             ),
-                            SizedBox(height: 10.h),
-                            Center(child: CircularProgressIndicator()),
                           ],
                         );
                       }
+
                       if (state is ProfileSuccess) {
                         currentImageUrl = state.profileModel.imageUrl;
                         userName = state.profileModel.name;
                         userId = state.profileModel.userId;
                       }
+
                       return Column(
                         children: [
-                          UserImageProfile(
-                            widgetUserImageProfile: CircleAvatar(
-                              radius: 46.r,
-                              backgroundImage:
-                                  currentImageUrl != null &&
-                                      currentImageUrl!.isNotEmpty
-                                  ? NetworkImage(
-                                      "$currentImageUrl?t=${DateTime.now().millisecondsSinceEpoch}",
-                                    )
-                                  : null,
-                              child:
-                                  currentImageUrl == null ||
-                                      currentImageUrl!.isEmpty
-                                  ? Icon(Icons.person, size: 40.sp)
-                                  : null,
+                          GestureDetector(
+                            onTap: () => _showImageSourcePicker(context),
+                            child: UserImageProfile(
+                              widgetUserImageProfile: CircleAvatar(
+                                radius: 46.r,
+                                backgroundImage: currentImageUrl != null && currentImageUrl!.isNotEmpty
+                                    ? NetworkImage("$currentImageUrl?t=${DateTime.now().millisecondsSinceEpoch}")
+                                    : null,
+                                child: currentImageUrl == null || currentImageUrl!.isEmpty
+                                    ? Icon(Icons.person, size: 40.sp)
+                                    : null,
+                              ),
                             ),
                           ),
                           SizedBox(height: 18.h),
                           Text(
                             userName,
-                            style: Theme.of(
-                              context,
-                            ).textTheme.displayLarge!.copyWith(fontSize: 24.sp),
+                            style: Theme.of(context).textTheme.displayLarge!.copyWith(fontSize: 24.sp),
                           ),
                           SizedBox(height: 10.h),
                           Text(
                             "Patient ID : #HE-$userId",
-                            style: Theme.of(
-                              context,
-                            ).textTheme.bodyMedium!.copyWith(fontSize: 14.sp),
+                            style: Theme.of(context).textTheme.bodyMedium!.copyWith(fontSize: 14.sp),
                           ),
                         ],
                       );
                     },
                   ),
+
                   SizedBox(height: 20.h),
+
                   SizedBox(
-                    width: 200,
-                    height: 45,
+                    width: 200.w,
+                    height: 45.h,
                     child: ElevatedButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        Navigator.pushNamed(context, AppRoutes.personalInformation).then((value) {
+                          if (value == true) context.read<ProfileCubit>().getProfileData();
+                        });
+                      },
                       style: ElevatedButton.styleFrom(
                         backgroundColor: ColorManager.darkBlue,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12.r),
-                        ),
-                        padding: EdgeInsets.zero,
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.r)),
                       ),
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          Icon(
-                            Iconsax.edit_2,
-                            size: 20.sp,
-                            color: Colors.white,
-                          ),
+                          Icon(Iconsax.edit_2, size: 20.sp, color: Colors.white),
                           SizedBox(width: 8.w),
-                          Text(appLocalizations.editProfile),
+                          Text(
+                            appLocalizations.editProfile,
+                            style: const TextStyle(color: Colors.white),
+                          ),
                         ],
                       ),
                     ),
                   ),
+
                   SizedBox(height: 30.h),
+
+                  // خيارات اللغة والإعدادات
                   BlocBuilder<AppLocalizationCubit, Locale>(
                     builder: (context, locale) {
                       final isArabic = locale.languageCode == 'ar';
                       return CustomProfileContainerItem(
                         onPressedNotification: () {},
-                        onPressedIconMyRental: () {
-                          Navigator.pushNamed(context, AppRoutes.myRental);
-                        },
+                        onPressedIconMyRental: () => Navigator.pushNamed(context, AppRoutes.myRental),
                         onPressedIconPersonalInformation: () {
-                          Navigator.pushNamed(
-                            context,
-                            AppRoutes.personalInformation,
-                          ).then((value) {
-                            if (value == true) {
-                              context.read<ProfileCubit>().getProfileData();
-                            }
+                          Navigator.pushNamed(context, AppRoutes.personalInformation).then((value) {
+                            if (value == true) context.read<ProfileCubit>().getProfileData();
                           });
                         },
-                        onPressedIconContactUs: () {
-                          Navigator.pushNamed(context, AppRoutes.contactUs);
-                        },
+                        onPressedIconContactUs: () => Navigator.pushNamed(context, AppRoutes.contactUs),
                         textLanguage: isArabic ? "العربية" : "English",
-                        onPressedLanguage: () {
-                          Navigator.pushNamed(
-                            context,
-                            AppRoutes.languageProfile,
-                          );
-                        },
+                        onPressedLanguage: () => Navigator.pushNamed(context, AppRoutes.languageProfile),
                       );
                     },
                   ),
+
                   SizedBox(height: 20.h),
+
+                  // زرار تسجيل الخروج
                   GestureDetector(
                     onTap: _showDialogLogOut,
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Icon(Iconsax.logout, color: ColorManager.red, size: 26),
+                        Icon(Iconsax.logout, color: ColorManager.red, size: 26.sp),
                         SizedBox(width: 8.w),
                         Text(
                           appLocalizations.log_out,
-                          style: Theme.of(context).textTheme.headlineMedium!
-                              .copyWith(color: ColorManager.red),
+                          style: Theme.of(context).textTheme.headlineMedium!.copyWith(color: ColorManager.red),
                         ),
                       ],
                     ),
@@ -213,31 +225,25 @@ class _ProfileTabState extends State<ProfileTab> {
     showDialog(
       context: context,
       barrierDismissible: false,
-      builder: (context) {
-        return AlertDialog(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadiusGeometry.circular(16.r),
+      builder: (context) => AlertDialog(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16.r)),
+        title: Text(appLocalizations.log_out),
+        content: Text(appLocalizations.are_you_sure_you_want_to_log_out),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: Text(appLocalizations.cancel),
           ),
-          title: Text(appLocalizations.log_out),
-          content: Text(appLocalizations.are_you_sure_you_want_to_log_out),
-          actions: [
-            TextButton(
-              onPressed: () => Navigator.pop(context),
-              child: Text(appLocalizations.cancel),
+          TextButton(
+            onPressed: () => Navigator.pushNamedAndRemoveUntil(
+              context,
+              AppRoutes.startScreen,
+              (route) => false,
             ),
-            TextButton(
-              onPressed: () {
-                Navigator.pushNamedAndRemoveUntil(
-                  context,
-                  AppRoutes.startScreen,
-                  (route) => false,
-                );
-              },
-              child: Text(appLocalizations.ok),
-            ),
-          ],
-        );
-      },
+            child: Text(appLocalizations.ok),
+          ),
+        ],
+      ),
     );
   }
 }
